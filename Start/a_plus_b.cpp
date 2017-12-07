da88bf4dabc0d4f8a97d2b186b85a7c76494b681
KuoPing Kao <igaozp@hotmail.com>
2017-12-07 12:58:45 +0800

    [Title]  A + B 问题 [Level] 入门 [Description] 给出两个整数a和b, 求他们的和, 但不能使用 + 等数学运算符

---
+++ Start/a_plus_b.cpp
@@ -0,0 +1,15 @@
+//
+// Created by igaozp on 2017/12/7.
+//
+
+class Solution {
+public:
+    /*
+     * @param : An integer
+     * @param : An integer
+     * @return: The sum of a and b
+     */
+    int aplusb(int a, int b) {
+        return a + b;
+    }
+};
